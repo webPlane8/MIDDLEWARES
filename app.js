d6d6cea682ae2b06419f510ee48a74164e99f102
webPlane8 <webplane8@gmail.com>
2024-11-27 17:02:24 +0500

    basic error handling done

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const ExpressError = require("./ExpressError");
 
 const port = 8080;
 
@@ -24,13 +25,17 @@ const port = 8080;
 //   next();
 // });
 
+let arr = [1, 2, 3];
+arr.sayHello = () => {
+  console.log("Asslam O Alaikum");
+};
+
 const checkToken = (req, res, next) => {
   let { token } = req.query;
   if (token === "givenaccess") {
     next();
-  } else {
-    res.send("access Denied");
   }
+  throw new ExpressError(401, "Access Denied");
 };
 
 app.get("/api", checkToken, (req, res) => {
@@ -40,6 +45,26 @@ app.get("/api", checkToken, (req, res) => {
 app.get("/", (req, res) => {
   res.send(`index route is working`);
 });
+
+app.get("/err", (req, res) => {
+  abcd = abcd;
+});
+
+app.get("/admin", (req, res) => {
+  throw new ExpressError(403, "Admin is forbidden");
+});
+
+app.use((err, req, res, next) => {
+  let { status = 500, message = "some error occured" } = err;
+  res.status(status).send(message);
+
+  // console.log(`-------ERROR-------`);
+  // res.send(err);
+  next(err);
+});
+
+// app.use("/err", (req, res) => {});
+
 app.listen(port, () => {
   console.log(`app is listening on port: ${port}`);
 });
